e5055275dc19a9f0ebaf4a22e8f21de5fb0d5261
camwhite <cameron2703@icloud.com>
2022-11-08 23:50:27 +0000

    Moved setup function to new file

--- map_test.go
+++ map_test.go
@@ -6,56 +6,13 @@ import (
 	"encoding/json"
 	"github.com/cloudevents/sdk-go/v2/event"
 	"github.com/stretchr/testify/assert"
-	"os"
 	"testing"
 	"time"
 )
 
-func setupTest(tb testing.TB) (func(tb testing.TB), *pubsub.Subscription) {
-	// Setup test
-	// Modify the PUBSUB_EMULATOR_HOST environment variable to point to the pubsub emulator
-	existingVal := os.Getenv("PUBSUB_EMULATOR_HOST")
-	err := os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
-	if err != nil {
-		tb.Fatalf("Error setting environment variable: %v", err)
-	}
-	// Create a pubsub client so we can create a topic and subscription
-	client, err := pubsub.NewClient(context.Background(), "serverless-mapreduce")
-	if err != nil {
-		tb.Fatalf("Error creating pubsub client: %v", err)
-	}
-	t, err := client.CreateTopic(context.Background(), "mapreduce-shuffler-6")
-	if err != nil {
-		tb.Fatalf("Error creating topic: %v", err)
-	}
-	subscription, err := client.CreateSubscription(context.Background(), "mapreduce-shuffler", pubsub.SubscriptionConfig{
-		Topic: t,
-	})
-	if err != nil {
-		tb.Fatalf("Error creating subscription: %v", err)
-	}
-
-	return func(tb testing.TB) {
-		// Teardown test
-		// Delete the subscription
-		if err := subscription.Delete(context.Background()); err != nil {
-			tb.Fatalf("Error deleting subscription: %v", err)
-		}
-		// Delete the topic
-		if err := t.Delete(context.Background()); err != nil {
-			tb.Fatalf("Error deleting topic: %v", err)
-		}
-		// Reset the PUBSUB_EMULATOR_HOST environment variable
-		err = os.Setenv("PUBSUB_EMULATOR_HOST", existingVal)
-		if err != nil {
-			tb.Fatalf("Error setting environment variable: %v", err)
-		}
-	}, subscription
-}
-
 func TestMapper(t *testing.T) {
 	// Setup test
-	teardown, subscription := setupTest(t)
+	teardown, subscription := setupTest(t, "mapreduce-shuffler-6")
 	defer teardown(t)
 	// Given
 	// Create a message
---
+++ testing.go
@@ -0,0 +1,50 @@
+package serverless_mapreduce
+
+import (
+	"cloud.google.com/go/pubsub"
+	"context"
+	"os"
+	"testing"
+)
+
+func setupTest(tb testing.TB, topicID string) (func(tb testing.TB), *pubsub.Subscription) {
+	// Setup test
+	// Modify the PUBSUB_EMULATOR_HOST environment variable to point to the pubsub emulator
+	existingVal := os.Getenv("PUBSUB_EMULATOR_HOST")
+	err := os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
+	if err != nil {
+		tb.Fatalf("Error setting environment variable: %v", err)
+	}
+	// Create a pubsub client so we can create a topic and subscription
+	client, err := pubsub.NewClient(context.Background(), "serverless-mapreduce")
+	if err != nil {
+		tb.Fatalf("Error creating pubsub client: %v", err)
+	}
+	t, err := client.CreateTopic(context.Background(), topicID)
+	if err != nil {
+		tb.Fatalf("Error creating topic: %v", err)
+	}
+	subscription, err := client.CreateSubscription(context.Background(), topicID, pubsub.SubscriptionConfig{
+		Topic: t,
+	})
+	if err != nil {
+		tb.Fatalf("Error creating subscription: %v", err)
+	}
+
+	return func(tb testing.TB) {
+		// Teardown test
+		// Delete the subscription
+		if err := subscription.Delete(context.Background()); err != nil {
+			tb.Fatalf("Error deleting subscription: %v", err)
+		}
+		// Delete the topic
+		if err := t.Delete(context.Background()); err != nil {
+			tb.Fatalf("Error deleting topic: %v", err)
+		}
+		// Reset the PUBSUB_EMULATOR_HOST environment variable
+		err = os.Setenv("PUBSUB_EMULATOR_HOST", existingVal)
+		if err != nil {
+			tb.Fatalf("Error setting environment variable: %v", err)
+		}
+	}, subscription
+}
