0693a920c5bd1d1d91a297fa5e2bf30d61b0afd9
camwhite <cameron2703@icloud.com>
2022-11-22 20:43:20 +0000

    Renamed files

--- reduce.go
+++ reduce.go
@@ -1,1 +1,64 @@
 package serverless_mapreduce
+
+import (
+	"context"
+	"fmt"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/cloudevents/sdk-go/v2/event"
+	"github.com/gomodule/redigo/redis"
+	"log"
+	"os"
+)
+
+func init() {
+	functions.CloudEvent("Reducer", reducer)
+}
+
+var redisPool *redis.Pool
+
+func initRedisPool() (*redis.Pool, error) {
+	redisHost := os.Getenv("REDIS_HOST")
+	if redisHost == "" {
+		return nil, fmt.Errorf("REDIS_HOST not set")
+	}
+	redisPort := os.Getenv("REDIS_PORT")
+	if redisPort == "" {
+		return nil, fmt.Errorf("REDIS_PORT not set")
+	}
+	redisAddress := fmt.Sprintf("%s:%s", redisHost, redisPort)
+
+	const maxConnections = 10
+	return &redis.Pool{
+		MaxIdle: maxConnections,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", redisAddress)
+		},
+	}, nil
+}
+
+func reducer(_ context.Context, e event.Event) error {
+	//Initialize the redis pool if it hasn't been initialized yet
+	if redisPool == nil {
+		var err error
+		redisPool, err = initRedisPool()
+		if err != nil {
+			return fmt.Errorf("error initializing redis pool: %v", err)
+		}
+	}
+
+	var wordDataSlice []WordData
+	_, _, err := ReadPubSubMessage(context.Background(), e, &wordDataSlice)
+	if err != nil {
+		return fmt.Errorf("error reading pubsub message: %v", err)
+	}
+	conn := redisPool.Get()
+	defer conn.Close()
+	// Combine and reduce in one step by storing the data in a set in redis
+	for _, wordData := range wordDataSlice {
+		_, err := conn.Do("SADD", wordData.SortedWord, wordData.Word)
+		if err != nil {
+			log.Printf("error pushing value to set in redis: %v", err)
+		}
+	}
+	return nil
+}
--- reduce_test.go
+++ reduce_test.go
@@ -7,8 +7,10 @@ import (
 	"github.com/cloudevents/sdk-go/v2/event"
 	"github.com/gomodule/redigo/redis"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"os"
 	"testing"
+	"time"
 )
 
 func SetupRedisTest(tb testing.TB) func(tb testing.TB) {
@@ -59,7 +61,7 @@ func SetupRedisTest(tb testing.TB) func(tb testing.TB) {
 	}
 }
 
-func TestShuffler(t *testing.T) {
+func TestReducer(t *testing.T) {
 	// Given
 	teardown, _ := SetupTest(t, "mapreduce-reducer-0")
 	defer teardown(t)
@@ -91,17 +93,74 @@ func TestShuffler(t *testing.T) {
 		t.Fatalf("Error setting event data: %v", err)
 	}
 
-	expectedResult := []string{"quick", "quick"}
+	expectedResult := []string{"quick"}
 
 	// When
-	err = shuffler(context.Background(), e)
+	startTime := time.Now()
+	err = reducer(context.Background(), e)
+	elapsedTime := time.Since(startTime)
+	log.Printf("Shuffler took %s", elapsedTime)
 
 	// Then
 	assert.Nil(t, err)
 	conn := redisPool.Get()
 	defer conn.Close()
 
-	li, err := conn.Do("LRANGE", wordDataSlice[0].SortedWord, 0, -1)
+	li, err := conn.Do("SMEMBERS", wordDataSlice[0].SortedWord)
+	if err != nil {
+		t.Fatalf("Error getting data from redis: %v", err)
+	}
+	var actualResult []string
+	for _, v := range li.([]interface{}) {
+		actualResult = append(actualResult, string(v.([]byte)))
+	}
+	assert.Equal(t, expectedResult, actualResult)
+}
+
+func TestReducerEfficiency(t *testing.T) {
+	// Given
+	teardown, _ := SetupTest(t, "mapreduce-reducer-0")
+	defer teardown(t)
+	teardownRedis := SetupRedisTest(t)
+	defer teardownRedis(t)
+	var wordDataSlice []WordData
+	for i := 0; i < 10000; i++ {
+		wordDataSlice = append(wordDataSlice, WordData{Word: "quick", SortedWord: "cikqu"})
+	}
+	// Create a message
+	wordDataBytes, err := json.Marshal(wordDataSlice)
+	if err != nil {
+		t.Fatalf("Error marshalling word data: %v", err)
+	}
+	message := MessagePublishedData{
+		Message: PubSubMessage{
+			Data:       wordDataBytes,
+			Attributes: map[string]string{"splitter": "0", "noOfReducers": "1"},
+		},
+	}
+
+	// Create a CloudEvent to be sent to the shuffler
+	e := event.New()
+	e.SetDataContentType("application/json")
+	err = e.SetData(e.DataContentType(), message)
+	if err != nil {
+		t.Fatalf("Error setting event data: %v", err)
+	}
+
+	expectedResult := []string{"quick"}
+
+	// When
+	startTime := time.Now()
+	err = reducer(context.Background(), e)
+	elapsedTime := time.Since(startTime)
+	log.Printf("Shuffler took %s", elapsedTime)
+
+	// Then
+	assert.Nil(t, err)
+	conn := redisPool.Get()
+	defer conn.Close()
+
+	li, err := conn.Do("SMEMBERS", wordDataSlice[0].SortedWord)
 	if err != nil {
 		t.Fatalf("Error getting data from redis: %v", err)
 	}
